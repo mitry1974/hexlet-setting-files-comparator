18a253fdd1552f5cc3451fec379ef375dfa2dc32
DT <tomitry@gmail.com>
2019-07-11 22:27:12 +0300

    code refactoring

--- src/index.js
+++ src/index.js
@@ -1,8 +1,57 @@
+import _ from 'lodash';
 import fs from 'fs';
 import path from 'path';
 import parse from './parsers';
 import format from './formatters';
-import generateAst from './generateAst';
+
+const diffOperations = [
+  {
+    check: (arg1, arg2) => _.isObject(arg1) && _.isObject(arg2),
+    // eslint-disable-next-line no-use-before-define
+    getOperation: (key, object1, object2) => [({ diffOp: 'group', key, children: generateAst(object1, object2) })],
+  },
+  {
+    check: (arg1, arg2) => arg1 === arg2 && typeof arg1 === typeof arg2,
+    getOperation: (key, arg1) => [({ diffOp: 'unchanged', key, value: arg1 })],
+  },
+  {
+    check: arg1 => !arg1,
+    getOperation: (key, object1, object2) => [({ diffOp: 'added', key, value: object2 })],
+  },
+  {
+    check: (arg1, arg2) => !arg2,
+    getOperation: (key, object1) => [({ diffOp: 'deleted', key, value: object1 })],
+  },
+  {
+    check: (arg1, arg2) => typeof arg1 !== typeof arg2,
+    getOperation: (key, object1, object2) => ([
+      {
+        diffOp: 'deleted', key, value: object1,
+      },
+      {
+        diffOp: 'added', key, value: object2,
+      }]
+    ),
+  },
+  {
+    check: (arg1, arg2) => arg1 !== arg2,
+    getOperation: (key, object1, object2) => [{
+      diffOp: 'changed', key, oldValue: object1, newValue: object2,
+    }],
+  },
+];
+
+const getDiffOperation = (arg1, arg2) => diffOperations.find(({ check }) => check(arg1, arg2));
+
+const unionKeys = (object1, object2) => _.union(_.keys(object1), _.keys(object2));
+
+const generateAst = (object1, object2) => {
+  const keys = unionKeys(object1, object2);
+  return keys.reduce((acc, key) => {
+    const { getOperation } = getDiffOperation(object1[key], object2[key]);
+    return [...acc, ...getOperation(key, object1[key], object2[key])];
+  }, []);
+};
 
 export default (filePath1, filePath2, formatterType) => {
   const parserType = path.extname(filePath1);
